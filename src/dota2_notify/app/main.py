f2bd7c2f3decc85418f074793ba3db65e0fb778c
Yunier Perez <yunier.soad@gmail.com>
2026-01-16 16:21:45 -0500

    Add fast api and example endpoints

--- src/dota2_notify/app/main.py
+++ src/dota2_notify/app/main.py
@@ -1,28 +1,43 @@
-import json
-from dota2_notify.models.match import DotaMatch
+import os
+from contextlib import asynccontextmanager
+from fastapi import FastAPI, HTTPException
+from dotenv import load_dotenv
+
+from dota2_notify.clients.cosmosdb_client import CosmosDbUserService 
+
+load_dotenv() 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    service = CosmosDbUserService(
+        connection_endpoint=os.getenv("COSMOSDB__ENDPOINTURI"),
+        key=os.getenv("COSMOSDB__PRIMARYKEY"),
+        database_name=os.getenv("COSMOSDB__DATABASENAME"),
+        container_name=os.getenv("COSMOSDB__CONTAINERNAME")
+    )
+    
+    async with service:
+        app.state.user_service = service
+        yield 
+    
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/users")
+async def get_users():
+    # Reuse the persistent, non-blocking connection
+    return await app.state.user_service.get_all_users_async()
+
+@app.get("/users/{user_id}")
+async def get_user(user_id: int):
+    # Reuse the persistent, non-blocking connection
+    user = await app.state.user_service.get_user_async(user_id)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
 
 def main():
-    payload = """{"match_id": 8642866804,
-        "player_slot": 3,
-        "radiant_win": true,
-        "duration": 2285,
-        "game_mode": 2,
-        "lobby_type": 1,
-        "hero_id": 100,
-        "start_time": 1768015812,
-        "version": 22,
-        "kills": 4,
-        "deaths": 5,
-        "assists": 21,
-        "average_rank": 75,
-        "leaver_status": 0,
-        "party_size": 10,
-        "hero_variant": 1
- }"""
-    match = DotaMatch.from_json(payload)
-    # print(match)
-    print(match.match_duration)
-
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
 if __name__ == "__main__":
     main()

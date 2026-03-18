e0170734adbed867a15d7afc7ed5351fa645779e
Yunier Perez <yunier.soad@gmail.com>
2026-03-18 18:37:23 -0500

    Remove old open dota  polling based notifications

--- src/dota2_notify/app/main.py
+++ src/dota2_notify/app/main.py
@@ -1,17 +1,12 @@
-from dota2_notify.app.config import Settings, get_settings
-from fastapi import Depends
-import os
+from dota2_notify.app.config import get_settings
 from contextlib import asynccontextmanager
 from fastapi import FastAPI, Request
 from dota2_notify.web import auth, health, friends, static, notifications
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from azure.cosmos.aio import CosmosClient
 import logging
 
 import httpx
-from dota2_notify.app.match_checker import check_new_matches
 from dota2_notify.clients.cosmosdb_client import CosmosDbUserService 
-from dota2_notify.clients.opendota_client import OpenDotaClient
 from dota2_notify.clients.telegram_client import TelegramClient
 from dota2_notify.clients.steam_client import SteamClient
 
@@ -50,26 +45,14 @@ async def lifespan(app: FastAPI):
     http_client = httpx.AsyncClient(
         event_hooks={'request': [log_request], 'response': [log_response]}
     )
-    open_dota_client = OpenDotaClient(client=http_client)    
     telegram_client = TelegramClient(token=settings.telegram_bot_token, client=http_client)
     steam_client = SteamClient(api_key=settings.steam_api_key,client=http_client)
     app.state.steam_client = steam_client
 
-    scheduler = AsyncIOScheduler()
-    if settings.matchcheck_enabled:
-        interval_minutes = settings.matchcheck_interval_minutes
-        logging.info(f"Match checking is enabled. Scheduling periodic match checks. Every {interval_minutes} minutes.")
-        scheduler.add_job(check_new_matches, 'interval', minutes=interval_minutes, args=[db_client, open_dota_client, telegram_client])
-        scheduler.start()
-    else:
-        logging.info("Match checking is disabled. No periodic match checks will be scheduled.")
-    
     # pass control to the application
     yield
 
     # cleanup
-    if settings.matchcheck_enabled:
-        scheduler.shutdown()
     await db_client.close()
     await http_client.aclose()
 

c225520a80aac27a4dacafadd36cf02eb479502a
Yunier Perez <yunier.soad@gmail.com>
2026-03-28 19:37:24 -0500

    Warning to let the user now they need to verify telegram

--- src/dota2_notify/web/dependencies.py
+++ src/dota2_notify/web/dependencies.py
@@ -6,6 +6,7 @@ from fastapi.templating import Jinja2Templates
 top = Path(__file__).resolve().parent
 template_obj = Jinja2Templates(directory=str(top / "templates"))
 template_obj.env.globals["now"] = datetime.now
+template_obj.env.globals['CSS_VERSION'] = "1.0.0"
 
 def get_user_service(request: Request):
     return request.app.state.user_service

73f07a38ac7dcadbf844dce772b6b3556e93e479
Yunier Perez <yunier.soad@gmail.com>
2026-08-20 20:31:50 -0500

    Add dotabuff link

--- src/dota2_notify/web/dependencies.py
+++ src/dota2_notify/web/dependencies.py
@@ -4,10 +4,13 @@ from fastapi import Request
 from fastapi.templating import Jinja2Templates
 from starlette.requests import HTTPConnection
 
+from dota2_notify.models.user import steam_id_to_account_id
+
 top = Path(__file__).resolve().parent
 template_obj = Jinja2Templates(directory=str(top / "templates"))
 template_obj.env.globals["now"] = datetime.now
 template_obj.env.globals['CSS_VERSION'] = "1.0.1"
+template_obj.env.filters["dotabuff_url"] = lambda steam_id: f"https://www.dotabuff.com/players/{steam_id_to_account_id(int(steam_id))}"
 
 def get_user_service(request: Request):
     return request.app.state.user_service

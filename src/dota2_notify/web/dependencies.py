76442ae052599a197809f778590b067f12991cd0
Yunier Perez <yunier.soad@gmail.com>
2026-08-26 21:38:32 -0500

    Bump CSS version

--- src/dota2_notify/web/dependencies.py
+++ src/dota2_notify/web/dependencies.py
@@ -9,7 +9,7 @@ from dota2_notify.models.user import steam_id_to_account_id
 top = Path(__file__).resolve().parent
 template_obj = Jinja2Templates(directory=str(top / "templates"))
 template_obj.env.globals["now"] = datetime.now
-template_obj.env.globals['CSS_VERSION'] = "1.0.1"
+template_obj.env.globals['CSS_VERSION'] = "1.0.2"
 template_obj.env.filters["dotabuff_url"] = lambda steam_id: f"https://www.dotabuff.com/players/{steam_id_to_account_id(int(steam_id))}"
 
 def get_user_service(request: Request):
